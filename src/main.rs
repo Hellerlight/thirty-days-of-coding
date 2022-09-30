6f45d4cbf626528bfead09f49c0077ce8f0062cb
Aleksandr <hellerlight@gmail.com>
2022-09-30 23:02:33 +0300

    Day_two

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,8 @@ use std::io::{Read, stdin};
 
 // Day one
 // Table drawer
-fn main() {
-    println!("Hello, world!");
-    let mut table_size_x:i32 = 15;
-    let mut table_size_y:i32 = 5;
-    drawtable(table_size_x, table_size_y)
-}
+const initial_table_size_x: i32 = 15;
+const initial_table_size_y: i32 = 5;
 fn drawtable_bot_top(table_x:i32) {
     print!(" ");
     for i in (0..table_x) {
@@ -18,7 +14,6 @@ fn drawtable_bot_top(table_x:i32) {
 }
 fn drawtable(table_x: i32, table_y:i32) {
     println!("Tablesize x:{}, y:{}", table_x, table_y);
-
     drawtable_bot_top(table_x);
     print!("\n");
     for i in (0..table_y) {
@@ -31,4 +26,28 @@ fn drawtable(table_x: i32, table_y:i32) {
     }
     drawtable_bot_top(table_x);
 }
+// Day Two
+fn addbar(name:&str, value:i32) -> String {
+    let mut resultstring:String = String::new();
+    resultstring.push_str(name);
+    for i in (0..value) {
+        resultstring.push_str("|");
+    }
+    return resultstring;
+}
 
+fn draw_bars(bars:Vec<String>) {
+    for i in bars.iter() { println!("{}", i); }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut table_databars:Vec<String> = Vec::new();
+    table_databars.push(addbar("ptat", 30));
+    table_databars.push(addbar("Bob", 15));
+    table_databars.push(addbar("Timothy", 9));
+    // drawtable(initial_table_size_x, initial_table_size_y);
+    draw_bars(table_databars);
+
+}
